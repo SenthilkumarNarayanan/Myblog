89a3ec6dac38bf2210daa007e722ec7332b9d89e
SenthilkumarNarayanan <124486793+SenthilkumarNarayanan@users.noreply.github.com>
2025-06-02 21:21:21 +0530

    WIP: Temporary commit before rebase

--- frontend/client/src/components/Footer.jsx
+++ frontend/client/src/components/Footer.jsx
@@ -5,17 +5,17 @@ import { assets, footer_data } from '../assets/assets'
 const Footer = () => {
   return (
     <div className='px- 6 md:px-16 bg-gray-100 lg:px-24 xl:px-32 bg:primary/3'>
-        <div className='flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500' >
+        <div className='flex flex-col md:flex-row items-center justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500' >
 
-        <div className='relative top-10'>
-            <img src={assets.logo} alt="" className='rounded-full w-50 relative bottom-10' />
+        <div className='relative top-10 flex flex-row md:flex-col gap-8 left-0 right-0 '>
+            <img src={assets.logo} alt="" className='rounded-full w-50 relative bottom-10 ' />
             <p className='max-w-[410px] relative bottom-10'>Lorem ipsum dolor sit, amet consectetur adipisicing 
             elit. Aperiam et odio provident, eius, 
             consequatur suscipit exercitationem assumenda
              veniam, magnam iste nostrum. Omnis, perspiciatis.
              Ex unde facilis sint ipsam! Perspiciatis, nam?</p>
         </div >
-        <div className='flex relative top-9 bottom-10flex-wrap justify-between w-full md:w-[45%] gap-5'>
+        <div className='flex relative  top-9  bottom-10 flex-wrap justify-between w-full md:w-[45%] gap-5'>
          {footer_data.map((footer, index)=>(
          <div key={index}>
             <h3 className='font-semibold text-base text gray-500 md:mb-5 mb-2'>{footer.title}</h3>
