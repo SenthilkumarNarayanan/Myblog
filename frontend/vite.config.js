41ba8ea459e8bc61844ace2823b5f99c9891bd11
SenthilkumarNarayanan <124486793+SenthilkumarNarayanan@users.noreply.github.com>
2025-06-03 15:35:54 +0530

    build the project to deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build/dist'
+  },
   plugins: [react(),
     tailwindcss()
   ],
